05e444750b02f0725c65fcb6f82e2ed38ceadc6d
manurajsiddhu <manurajsiddhu@gmail.com>
2026-01-18 09:07:45 +0530

    day 2

---
+++ user_input.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#this script is for taken user input and print
+
+read -p "enter you name :" name
+read -p "enter your age :" age 
+read -p "enter your goal :" goal
+read -p "enter you are male or female :" gender
+
+#print the values 
+
+echo "you name is $name, you are $age year old $gender,And your goal is $goal"
+
